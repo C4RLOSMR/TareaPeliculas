a05268211bca294a9b8d08f21cb6d7c8b6013797
C4RLOSMR <124205505+C4RLOSMR@users.noreply.github.com>
2023-03-21 21:22:33 -0700

    constructor Actor

--- Pelicula/Program.cs
+++ Pelicula/Program.cs
@@ -9,7 +9,8 @@ namespace Pelicula
         //Campos
         private string titulo, director, pais;
         private Int16 año;
-    
+
+
         public string GetTitulo(){return titulo;}
         public string GetDirector(){return director;}
         public string GetPais(){return pais;}
@@ -30,13 +31,13 @@ namespace Pelicula
                
         }
 
-/*
+
         private List<Actor> actores = new List<Actor>();
 
         //Constructores
      
         //Métodos
-     */
+     
         public void Imprime()
         {
      //       Console.WriteLine($"{titulo} ({año})");
@@ -47,20 +48,31 @@ namespace Pelicula
     }
 
 
-/*
+
     public class Actor
     {
         //Propiedades
+       private string Nombre;
+       private  Int16 Año;
+
+        public string Getnombre(){return Nombre;}
+        public Int16 Getaño(){return Año;}
+        public void Setnombre(string Nuevonombre){Nombre = Nuevonombre;}
+        public void SetAño(Int16 Nuevoaño){ Año = Nuevoaño;}
 
         //Constructores
-
+        public Actor(string a, Int16 b)
+        {
+            Nombre = a;
+            Año = b;
+        }
 
         //Métodos 
         public void Imprime()
         {
             Console.WriteLine($"{Nombre} ({Año})");
         }
-    }*/
+    }
 
     // Puedes probar tu código en Main() pero lo importante
     // es que pase las pruebas
@@ -71,6 +83,7 @@ namespace Pelicula
         {
             Pelicula p1 = new Pelicula();
             p1.SetTitulo("THE GODFATHER");
+           
             p1.SetAño(1972);
             Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine("{0}({1})", p1.GetTitulo(), p1.GetAño());
@@ -89,7 +102,6 @@ namespace Pelicula
             peliculas.Add("Crossroads");
             peliculas.Add("Reservoir Dogs");
             peliculas.Add("Blade Runner 2049");
-
             Console.ForegroundColor = ConsoleColor.Green;
             foreach(var pelicula in peliculas)
             {
